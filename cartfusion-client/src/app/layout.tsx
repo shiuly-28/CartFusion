eda8632935daa87a8bc9bf742a490958ea702363
shiuly-28 <shulybd1245@gmail.com>
2026-07-14 11:44:57 +0600

    favicon changed and uploaded all client files directly

---
+++ cartfusion-client/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+
+
+export const metadata: Metadata = {
+  title: "CartFusion",
+  description: "Multi-vendor e-commerce website",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html
+      lang="en"
+      
+    >
+      <body className="">
+        {children}
+        </body>
+    </html>
+  );
+}
---
+++ cartfusion-client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+      hkhjkjh
+    </div>
+  )
+}
+
+export default page
