7169e9974fc6884cc9c964e288d77941da98e8da
shiuly-28 <shulybd1245@gmail.com>
2026-07-19 00:00:06 +0600

    global file set up

--- cartfusion-client/src/lib/connectDB.ts
+++ cartfusion-client/src/lib/connectDB.ts
@@ -1,6 +1,13 @@
+import mongoose  from 'mongoose';
 const mongoDBUrl = process.env.MONGODB_URL
 
 
 if(!mongoDBUrl){
-    
+    throw new Error("DB error")
+}
+
+let cached = global.mongoose
+
+if(!cached){
+    cached = global.mongoose = {conn:null, promise:null}
 }
