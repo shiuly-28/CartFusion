8f7870c6fce3f949598c9b6572a4b9c59fcd50d5
shiuly-28 <shulybd1245@gmail.com>
2026-07-19 17:23:41 +0600

    db connection done

--- cartfusion-client/src/lib/connectDB.ts
+++ cartfusion-client/src/lib/connectDB.ts
@@ -11,3 +11,20 @@ let cached = global.mongoose
 if(!cached){
     cached = global.mongoose = {conn:null, promise:null}
 }
+
+const connectDb = async () =>{
+    if(cached.conn){
+        return cached.conn
+    }
+    if(!cached.promise){
+        cached.promise = mongoose.connect(mongoDBUrl).then((conn) =>conn.connection)
+    }
+    try{
+        const conn = await cached.promise
+        return conn
+    }catch (error){
+        console.log(error)
+    }
+}
+
+export default connectDb;
