cd604bbbea38c26db0894b0e201af3932d70e95e
shiuly-28 <shulybd1245@gmail.com>
2026-07-19 19:55:00 +0600

    route file error fixed

--- cartfusion-client/src/app/api/auth/register/route.ts
+++ cartfusion-client/src/app/api/auth/register/route.ts
@@ -1,16 +1,40 @@
 import connectDb from "@/lib/connectDB";
 import User from "@/model/user.model";
+import bcrypt from "bcryptjs";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest){
-    connectDb()
+    try{
+         await  connectDb()
     const {name, email, password} =await req.json()
 
-    const existUser = await User.find({email})
+    const existUser = await User.findOne({email})
 
     if(existUser){
         return NextResponse.json({
             message:"User is already exist"
-        },)
+        },{status:400})
     }
+    if(password.length < 6){
+        return NextResponse.json({
+            message:"password must be atleast six characters"
+        }, {status:400})
+    }
+
+    const hasPassword = await bcrypt.hash(password, 10)
+    const user = await User.create({
+        name,
+        email,
+        password
+    })
+    return NextResponse.json({
+            user
+        }, {status:201})
+
+    }catch(error){
+            return NextResponse.json({
+            message: `register error ${error}`
+        }, {status:500})
+    }
+ 
 }
