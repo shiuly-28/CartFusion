f1cc20ae27a53d48d067cd96e3bf4fc105ecb896
shiuly-28 <shulybd1245@gmail.com>
2026-07-20 23:57:44 +0600

    animation add

--- cartfusion-client/src/app/register/page.tsx
+++ cartfusion-client/src/app/register/page.tsx
@@ -1,12 +1,24 @@
 "use client"
 import React, { useState } from 'react'
+import { AnimatePresence, motion } from "motion/react"
 
 function Register() {
     const [step, setStep] = useState<1 | 2>()
   return (
     <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 
     via-black to-gray-900 text-white p-6'>
-      
+
+        <AnimatePresence mode='wait'>
+      {step == 1 && <motion.div 
+      className='w-full max-w-lg text-center bg-white/10 backdrop-blur-md rounded-2xl
+      shadow-2xl p-10 border border-white/20'>
+        
+      </motion.div>}
+
+      {step == 2 &&<motion.div>
+
+      </motion.div>}
+      </AnimatePresence>
     </div>
   )
 }
