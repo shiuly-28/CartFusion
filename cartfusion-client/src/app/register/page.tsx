63d2ac82883a1ab4de171706cb063b014b243cf6
shiuly-28 <shulybd1245@gmail.com>
2026-07-20 23:43:12 +0600

    register page implement

---
+++ cartfusion-client/src/app/register/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+
+function Register() {
+    const [step, setStep] = useState<1 | 2>()
+  return (
+    <div className='min-h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 
+    via-black to-gray-900 text-white p-6'>
+      
+    </div>
+  )
+}
+
+export default Register
