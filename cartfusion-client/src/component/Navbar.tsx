90dd31a423789e8b1646a4f2540fa188b28bb6c6
shiuly-28 <shulybd1245@gmail.com>
2026-08-05 01:52:52 +0600

    responsive setup

--- cartfusion-client/src/component/Navbar.tsx
+++ cartfusion-client/src/component/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from 'next/image';
 import { useRouter } from 'next/navigation'
 import { AnimatePresence, motion } from "motion/react"
 import logo from '@/assets/logo.jpg.png'
-import { AiOutlineLogin, AiOutlineLogout, AiOutlinePhone, AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
+import { AiOutlineLogin, AiOutlineLogout, AiOutlineMenu, AiOutlinePhone, AiOutlineSearch, AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
 import { IconType } from 'react-icons';
 import { signOut } from 'next-auth/react';
 import { AppRouterInstance } from 'next/dist/shared/lib/app-router-context.shared-runtime';
@@ -38,6 +38,7 @@ interface CartBtnProps {
 function Navbar({ user }: { user: IUser }) {
   const router = useRouter();
   const [openMenu, setOpenMenu] = useState(false);
+  const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
     <div className='fixed top-0 left-0 w-full bg-black text-white z-50 shadow-lg'>
@@ -108,6 +109,59 @@ function Navbar({ user }: { user: IUser }) {
             
           </div>
           {user?.role == "user" && <CartBtn router={router} count={5}/>}
+        </div>
+        {/* mobile icon */}
+        <div>
+          {user?.role == "merchant" || user?.role == "admin" ? (
+            <>
+            <IconBtn Icon={AiOutlinePhone} onClick={() => router.push("/support")}/>
+           <div className='relative'>
+            {user?.image ? (
+              <Image 
+                src={user.image} 
+                alt='user' 
+                width={32} 
+                height={32}
+                className='w-8 h-8 rounded-full object-cover border border-gray-700 cursor-pointer' 
+                onClick={() => setOpenMenu(!openMenu)}
+              />
+            ) : user?.name ? (
+              <div 
+                onClick={() => setOpenMenu(!openMenu)} 
+                className='w-8 h-8 rounded-full bg-[#049770] text-white font-semibold flex items-center justify-center cursor-pointer uppercase'
+              >
+                {user.name.trim()[0]}
+              </div>
+            ) : (
+              <IconBtn Icon={AiOutlineUser} onClick={() => setOpenMenu(!openMenu)}/>
+            )}
+
+            <AnimatePresence>
+              {openMenu && (
+                <motion.div 
+                  initial={{ opacity: 0, y: -10 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  exit={{ opacity: 0, y: -10 }}
+                  transition={{ duration: 0.3 }}
+                  className='absolute right-0 mt-3 w-48 backdrop-blur-lg rounded-xl shadow-lg border bg-[#6a69693c] p-2'
+                >
+                  <DropDownBtn Icon={AiOutlineUser} label="Profile" onClick={()=>{router.push("/profile"); setOpenMenu(false)}}/>
+                  <DropDownBtn Icon={AiOutlineLogin} label="SignIn" onClick={()=>{router.push("/login"); setOpenMenu(false)}}/>
+                  <DropDownBtn Icon={AiOutlineLogout} label="SignOut" onClick={()=>{signOut; setOpenMenu(false)}}/>
+                </motion.div>
+              )}
+            </AnimatePresence>
+            
+          </div>
+            </>
+          ):(
+            <>
+           <IconBtn Icon={AiOutlineSearch} onClick={() => router.push("/category")}/>
+           <IconBtn Icon={AiOutlinePhone} onClick={() => router.push("/support")}/>
+            <CartBtn router={router} count={5}/>
+            <AiOutlineMenu/>
+            </>
+          )}
         </div>
       </div>
     </div>
