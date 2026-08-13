1a0029cce11390a14ad373a0bfdc9e2e4dcae429
shiuly-28 <shulybd1245@gmail.com>
2026-08-13 23:21:46 +0600

    global css updated

---
+++ cartfusion-client/src/component/User/Slider.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import slider from '@/assets/slider.avif'
+import slider1 from '@/assets/slider1.webp'
+
+
+function Slider() {
+  return (
+    <div className='relative w-full min-h-[90vh] mt-0 overflow-hidden bg-black text-white md:mt-[60px] pt-0 top-0'>
+      
+    </div>
+  )
+}
+
+export default Slider
