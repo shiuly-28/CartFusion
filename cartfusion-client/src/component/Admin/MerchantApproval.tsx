52a8e89892f203ab544050310833c4175a2ee1dd
shiuly-28 <shulybd1245@gmail.com>
2026-08-24 00:57:55 +0600

    merchant button fixed

--- cartfusion-client/src/component/Admin/MerchantApproval.tsx
+++ cartfusion-client/src/component/Admin/MerchantApproval.tsx
@@ -2,14 +2,17 @@
 
 import { IUser } from '@/model/user.model'
 import { RootState } from '@/redux/store'
-import { tr } from 'motion/react-client'
-import React from 'react'
+import { AnimatePresence, motion } from 'motion/react'
+import { div, tr } from 'motion/react-client'
+import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
 function MerchantApproval() {
   const allMerchantData: IUser[] = useSelector((state:RootState)=>state.merchant.AllMerchantData)
   const pendingMerchant = Array.isArray(allMerchantData)?
     allMerchantData.filter((v)=>v.verificationStatus === "pending") : []
+
+    const [selectedMerchant, setSelectedMerchant] = useState<IUser | null>(null)
   
 
     console.log(pendingMerchant)
@@ -45,7 +48,9 @@ function MerchantApproval() {
                 <td className='p-4'><span className='px-3 py-1 rounded-full text-xs bg-yellow-500/50 text-yellow-300'>
                 {merchant.verificationStatus}</span></td>
                 <td className='p-4 text-center'>
-                  <button className='px-4 py-1 rounded-md bg-[#00684D] hover:bg-[#045f47]'>Check Details</button>
+                  <button
+                  onClick={() =>setSelectedMerchant(merchant)}
+                  className='px-4 py-1 rounded-md bg-[#00684D] hover:bg-[#045f47]'>Check Details</button>
                 </td>
               </tr>
              ))
@@ -53,6 +58,39 @@ function MerchantApproval() {
           </tbody>
         </table>
       </div>
+      {/* mobile card */}
+      <div className='md:hidden flex flex-col gap-4'>
+            {pendingMerchant.length === 0 ?(
+              <div className='text-center text-gray-400 mt-10'>
+                No Merchant Approval requests found
+              </div>
+            ) : (
+             pendingMerchant.map((merchant, index) => (
+               <div key={index} className='bg-white/10 border border-white/20 rounded-xl p-4 space-y-2'>
+                <div className='flex justify-between items-center'>
+                  <h3 className='font-semibold text-lg'>{merchant?.name}</h3>
+                  <span className='px-3 py-1 rounded-full text-xs bg-yellow-500/50
+                  text-yellow-300'>{merchant?.verificationStatus}</span>
+                </div>
+                <p className='text-sm text-gray-300'>
+                  <b>Shop:</b>{" "}{merchant.shopName}
+                </p>
+                <p className='text-sm text-gray-300'>
+                  <b>Phone:</b>{" "}{merchant.shopName}
+                </p>
+                <button className='w-full mt-3 bg-[#00684D] hover:bg-[#045f47]
+                text-sm py-2 rounded-lg' onClick={() => setSelectedMerchant(merchant)}>Check Details</button>
+               </div>
+             ))
+            )}
+      </div>
+      <AnimatePresence>
+        {selectedMerchant && (
+          <motion.div>
+
+          </motion.div>
+        )}
+      </AnimatePresence>
     </div>
   )
 }
